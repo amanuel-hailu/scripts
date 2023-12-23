ca7863425350555507dec39bd7c1522d82c0d6cc
Amanuel Hailu <aman-mac-work@Amanuels-MacBook-Pro.local>
2023-12-23 14:27:48 -0500

    Add work aliases and update personal aliases

---
+++ work/coe-uipath/uipath-connect-to-vm/connect-vpn-and-vm.py
@@ -0,0 +1,6 @@
+open -a "/Applications/Remote Desktop Manager.app" "rdm://open?DataSource=208da11f-4a8c-465a-921c-dfb607c2be91&Session=6ee82aa1-10fc-46b4-a805-9414361769fd"
+
+
+open -a "/Applications/Remote Desktop Manager.app" "rdm://open?DataSource=208da11f-4a8c-465a-921c-dfb607c2be91&Session=6ee82aa1-10fc-46b4-a805-9414361769fd"
+
+# Path: work/coe-uipath/uipath-connect-to-vm/connect-vpn-and-vm.py
